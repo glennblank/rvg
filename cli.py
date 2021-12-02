5f3d78069a131bfd351096696c11d03ea3897759
Glenn D. Blank <glennblank@gmail.com>
2021-12-02 15:58:47 -0500

    Refine CLI parameters

--- cli.py
+++ cli.py
@@ -3,11 +3,8 @@
 import click
 import rvg
 
-# ./cli.py --help
-# ./cli.py --step
-
 @click.command()
-@click.option('--step', is_flag=True, help='Show more details about matching productions')
+@click.option('--step', '-s', is_flag=True, default=False, help='Show more details about matching productions')
 def cli(step):
     print("Step is true!" if step else "Step is false!")
     rvg.rvg(step)
